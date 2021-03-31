59a41612d58f20f981199256ab6ec3ebb014b869
Victor Avdeev <avdeevvictor95@gmail.com>
2021-03-31 18:56:01 +0300

    fix: fix route to backend

--- src/api/Const.ts
+++ src/api/Const.ts
@@ -1,5 +1,5 @@
-// const BACKEND_ROUTE = 'https://react-learnwords-example.herokuapp.com';// remote
-const BACKEND_ROUTE = 'http://localhost:3005';// local
+const BACKEND_ROUTE = 'https://react-learnwords-example.herokuapp.com';// remote
+// const BACKEND_ROUTE = 'http://localhost:3005';// local
 const SIGNIN = '/signin';
 const USERS = '/users';
 
