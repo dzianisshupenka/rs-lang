c3af57a59d9144bcb5f6b17210cdd3fb22b639c0
Victor Avdeev <avdeevvictor95@gmail.com>
2021-04-01 00:42:12 +0300

    fead: add autologin with stored refreshToken

--- src/api/Const.ts
+++ src/api/Const.ts
@@ -1,6 +1,9 @@
-const BACKEND_ROUTE = 'https://react-learnwords-example.herokuapp.com';// remote
+const BACKEND_ROUTE = 'https://rs-lang-team65.herokuapp.com';// remote
 // const BACKEND_ROUTE = 'http://localhost:3005';// local
 const SIGNIN = '/signin';
 const USERS = '/users';
+const TOKENSLOGIN = '/tokenslogin';
 
-export { BACKEND_ROUTE, SIGNIN, USERS };
+export {
+  BACKEND_ROUTE, SIGNIN, USERS, TOKENSLOGIN,
+};
